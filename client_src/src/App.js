ddbe0afe932acd0595216aaea8639d22d22d2b62
Ana Nikolova <ani.nik19@gmail.com>
2020-10-12 08:56:15 +0200

    Navbar done

--- client_src/src/App.js
+++ client_src/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
+import { Link } from "react-router-dom";
 import Main from "./components/Main";
+import Navbar from "./components/Navbar";
 
 const App = () => (
   <div>
-    <Main />
+    <Navbar />
+    <div className="container">
+      <Main />
+    </div>
+    <div className="fixed-action-btn">
+      <Link to="/meetups/add" className="btn-floating btn-large red">
+        <div className="fa fa-plus"></div>
+      </Link>
+    </div>
   </div>
 );
 
